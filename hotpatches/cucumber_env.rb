df0217a5f6be748637bd3f3f3d9b5f8e6fbb8c82
mdkaraman <mdkaramanov@gmail.com>
2023-12-20 19:11:49 -0500

    require axe_cucumber_steps in hotpatch

--- hotpatches/cucumber_env.rb
+++ hotpatches/cucumber_env.rb
@@ -7,6 +7,7 @@
 # files.
 
 ENV["RAILS_ENV"] ||= 'test'
+
 puts "------------------------------------------------------------------------"
 puts "RAILS_ENV: #{ENV['RAILS_ENV']}"
 puts "------------------------------------------------------------------------"
@@ -25,6 +26,7 @@ require 'email_spec/cucumber'
 require 'rspec/expectations'
 # require 'capybara-screenshot/cucumber'
 require 'cucumber/rspec/doubles'
+require 'axe-cucumber-steps'
 
 Dir[File.expand_path("#{Rails.root}/lib/test/**/*.rb")].each { |f| load f }
 require File.expand_path("#{File.dirname(__FILE__)}/../../config/environment")
